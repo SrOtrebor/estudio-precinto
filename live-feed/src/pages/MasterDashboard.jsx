62ff0719aa9267ba7ef02b1716273f7368fcb970
Roberto Andres Laforcada <Rlaforcada@irsacorp.com.ar>
2026-08-04 15:33:03 -0300

    Fix useRef error in MasterDashboard

--- live-feed/src/pages/MasterDashboard.jsx
+++ live-feed/src/pages/MasterDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { db, ref, set, get, onValue, remove, storage, storageRef, uploadBytes, getDownloadURL } from "../firebase";
 
